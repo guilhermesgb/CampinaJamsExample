c345ffd81afea152c923b759786c25946b29bd5f
Guilherme Baptista <guisgb13@gmail.com>
2016-04-15 20:13:58 -0300

    Adiciona mensagem informando que não há itens salvos.

--- app/src/main/java/com/github/campinajams/example/MainActivity.java
+++ app/src/main/java/com/github/campinajams/example/MainActivity.java
@@ -2,13 +2,18 @@ package com.github.campinajams.example;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView noSavedItemsView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        noSavedItemsView = (TextView) findViewById(R.id.no_saved_items);
+        noSavedItemsView.setSelected(true);
     }
 
 }
